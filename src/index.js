462ac861d11dcb7a03fc5cc1be23ad11f1a6b559
Andy <andranadu@gmail.com>
2018-08-27 22:12:27 +0100

    Cleanup
    Hell yeah

--- src/index.js
+++ src/index.js
@@ -2,19 +2,8 @@
 const getHTML = require('./getPageHTML.js');
 const JapaneseData = require('./japaneseData.js');
 
-/*(
-    async (text) => new JapaneseData(await getHTML(text))
-)('業務すれ小谷お世ております。').then(j => {
-    console.log(j.original);
-    console.log(j.gloss);
-    console.log(j.romajiInfo);
-    console.log(j.romaji);
-    console.log(j.hiragana);
-    console.log(j.katakana);
-});*/
-
 module.exports = async (text) => new JapaneseData(await getHTML(text));
 
-module.exports('業務すれ小谷お世ております。').then(j => {
+/*module.exports('業務すれ小谷お世ております。').then(j => {
     console.log(JSON.stringify(j));
-});
+});*/
